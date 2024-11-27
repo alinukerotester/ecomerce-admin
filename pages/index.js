06626885adbe3b9a65e496877529f37c89c58094
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2024-11-27 17:53:19 +0200

    mai multe comenzi si implementari

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">
-          Login with google
-        </button>
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white p-2 px-4 rounded-lg"
+          >
+            <span>Login with </span>
+            <span className="google-logo">
+              {"Google".split("").map((letter, index) => (
+                <span key={index} className={`letter letter-${index % 4}`}>
+                  {letter}
+                </span>
+              ))}
+            </span>
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <div>logged in {session.user.email}</div>;
 }
