5211371e8da7c079222ed1f88bb2365ce84fb62f
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2024-11-28 17:29:45 +0200

    casute active

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,10 +1,34 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 export default function NewProduct() {
+  const [tittle, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
   return (
     <Layout>
-      <input type="text" placeholder="product name" />
-      <textarea placeholder="description"></textarea>
+      <h1>New Product</h1>
+      <label>Product name</label>
+      <input
+        type="text"
+        placeholder="product name"
+        value={tittle}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <label>Description</label>
+      <textarea
+        placeholder="description"
+        value={description}
+        onChange={(ev) => setDescription(ev.target.value)}
+      />
+      <label>Price (in USD)</label>
+      <input
+        type="number"
+        placeholder="price"
+        value={price}
+        onChange={(ev) => setPrice(ev.target.value)}
+      />
+      <button className="btn-primary">Save</button>
     </Layout>
   );
 }
