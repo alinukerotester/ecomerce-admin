825c3bac5e1dc78750003b9e4224648b8140bb10
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2024-12-02 13:17:57 +0200

    pagini noi

--- pages/products/edit/[...id].js
+++ pages/products/edit/[...id].js
@@ -7,6 +7,9 @@ export default function EditProductPage() {
   const router = useRouter();
   const { id } = router.query;
   useEffect(() => {
+    if (!id) {
+      return;
+    }
     axios.get("/api/products?id=" + id).then((response) => {
       console.log(response.data);
     });
