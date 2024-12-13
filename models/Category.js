54a845623e453586b3b38df9ae07f511f66e0aa1
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2024-12-13 16:31:12 +0200

    cam atat pentru azi

--- models/Category.js
+++ models/Category.js
@@ -1,5 +1,8 @@
-import { Schema } from 'mongoose';
+import mongoose, { model, models, Schema } from 'mongoose';
 
 const CategorySchema = new Schema({
 	name: { type: String, required: true },
+	parent: { type: mongoose.Types.ObjectId, ref: 'Category' },
 });
+
+export const Category = models?.Category || model('Category', CategorySchema);
