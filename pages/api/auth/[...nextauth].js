54aba0e9094369abca1b60b46e010bdcfe1083f1
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2024-12-17 12:40:25 +0200

    securitate adaugata

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,34 @@
-import clientPromise from "@/lib/mongodb";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import clientPromise from '@/lib/mongodb';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import NextAuth, { getServerSession } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-  ],
-  adapter: MongoDBAdapter(clientPromise),
-});
+const adminEmails = process.env.YO_M_L;
+
+export const authOptions = {
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_SECRET,
+		}),
+	],
+	adapter: MongoDBAdapter(clientPromise),
+	callbacks: {
+		session: ({ session, token, user }) => {
+			if (adminEmails.includes(session?.user?.email)) {
+				return session;
+			} else {
+				return false;
+			}
+		},
+	},
+};
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req, res) {
+	const session = await getServerSession(req, res, authOptions);
+	if (!adminEmails.includes(session?.user?.email)) {
+		throw 'not eu bă băiete';
+	}
+}
