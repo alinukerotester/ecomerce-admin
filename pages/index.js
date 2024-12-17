667d3f03b9572ea255699182f2377a68c9222e7d
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2024-12-17 14:31:46 +0200

    ajustari pretier

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,26 @@
-import Layout from "@/components/Layout";
-import { useSession } from "next-auth/react";
-import Image from "next/image";
+import Layout from '@/components/Layout';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
 
 export default function Home() {
-  const { data: session } = useSession();
-  return (
-    <Layout>
-      <div className="text-blue-900 flex justify-between">
-        <h2>
-          Hello, <b>{session?.user?.name}</b>
-        </h2>
-        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
-          <Image
-            src={session?.user?.image}
-            alt="poza"
-            width={96}
-            height={96}
-            className="w-16 h-16"
-          />
-          <span className="px-2">{session?.user?.name}</span>
-        </div>
-      </div>
-    </Layout>
-  );
+	const { data: session } = useSession();
+	return (
+		<Layout>
+			<div className='text-blue-900 flex justify-between'>
+				<h2>
+					Hello, <b>{session?.user?.name}</b>
+				</h2>
+				<div className='flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden'>
+					<Image
+						src={session?.user?.image}
+						alt='poza'
+						width={96}
+						height={96}
+						className='w-16 h-16'
+					/>
+					<span className='px-2'>{session?.user?.name}</span>
+				</div>
+			</div>
+		</Layout>
+	);
 }
