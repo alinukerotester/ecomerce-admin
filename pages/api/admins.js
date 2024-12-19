0113c905617ff9dfaf67fb58a4f453e9832ad614
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2024-12-19 15:47:48 +0200

    problema rezolvata

--- pages/api/admins.js
+++ pages/api/admins.js
@@ -1,5 +1,6 @@
 import { mongooseConnect } from '../../lib/mongoose';
 import Admin from '../../models/Admin';
+import { isAdminRequest } from './auth/[...nextauth]';
 
 export default async function handler(req, res) {
 	const { method } = req;
