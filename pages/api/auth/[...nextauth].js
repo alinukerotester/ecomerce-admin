681ce582ceb8780e55ac95d5dc8761dda571cdaf
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2024-12-23 13:02:14 +0200

    gata securitate si autentificare functionala

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,23 @@ import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth, { getServerSession } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-const adminEmails = process.env.YO_M_L;
+// Funcție pentru a obține emailurile administratorilor din baza de date
+async function getAdminEmails() {
+	const client = await clientPromise;
+	const db = client.db();
+	const adminsCollection = db.collection('admins');
+	const admins = await adminsCollection.find({}).toArray();
+	return admins.map((admin) => admin.email); // Asigură-te că emailurile sunt stocate în câmpul "email"
+}
+
+// Funcție pentru a combina emailurile din variabila de mediu și baza de date
+async function getCombinedAdminEmails() {
+	const dbAdminEmails = await getAdminEmails();
+	const envAdminEmails = process.env.YO_M_L
+		? process.env.YO_M_L.split(',')
+		: []; // Emailurile din variabila de mediu
+	return [...new Set([...dbAdminEmails, ...envAdminEmails])]; // Combină și elimină duplicatele, dacă există
+}
 
 export const authOptions = {
 	providers: [
@@ -14,7 +30,8 @@ export const authOptions = {
 	],
 	adapter: MongoDBAdapter(clientPromise),
 	callbacks: {
-		session: ({ session, token, user }) => {
+		session: async ({ session, token, user }) => {
+			const adminEmails = await getCombinedAdminEmails();
 			if (adminEmails.includes(session?.user?.email)) {
 				return session;
 			} else {
@@ -28,6 +45,8 @@ export default NextAuth(authOptions);
 
 export async function isAdminRequest(req, res) {
 	const session = await getServerSession(req, res, authOptions);
+	const adminEmails = await getCombinedAdminEmails();
+
 	if (!adminEmails.includes(session?.user?.email)) {
 		res
 			.status(401)
