cf32a8ea5d813c2da5f666f9ad0b740b5d7e3941
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-01-27 12:53:40 +0200

    o mica problema de css

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,10 @@
 import Nav from '@/components/Nav';
 import { useSession, signIn } from 'next-auth/react';
+import { useState } from 'react';
+import Logo from './Logo';
 
 export default function Layout({ children }) {
+	const [showNav, setShowNav] = useState(false);
 	const { data: session } = useSession();
 	if (!session) {
 		return (
@@ -25,10 +28,32 @@ export default function Layout({ children }) {
 	}
 
 	return (
-		<div className='bg-bgGray min-h-screen flex'>
-			<Nav />
-			<div className='bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4'>
-				{children}
+		<div className='bg-bgGray min-h-screen'>
+			<div className='block md:hidden flex items-center justify-center p-4'>
+				<button onClick={() => setShowNav(true)}>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						fill='none'
+						viewBox='0 0 24 24'
+						strokeWidth={1.5}
+						stroke='currentColor'
+						className='size-6'>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+						/>
+					</svg>
+				</button>
+				<div className='flex-grow justify-center mr-6'>
+					<Logo />
+				</div>
+			</div>
+			<div className='flex'>
+				<Nav show={showNav} />
+				<div className='bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4'>
+					{children}
+				</div>
 			</div>
 		</div>
 	);
