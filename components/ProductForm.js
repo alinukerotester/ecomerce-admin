2ec86742cf9ed0944be7263eeeee2e3787348764
RĂILEANU ALIN <109532807+alinukerotester@users.noreply.github.com>
2025-02-14 15:48:44 +0200

    o mica problema la poze

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -120,17 +120,19 @@ export default function ProductForm({
 			</select>
 			{propertiesToFill.length > 0 &&
 				propertiesToFill.map((p, index) => (
-					<div key={index} className='flex gap-1'>
-						<div>{p.name}</div>
-						<select
-							value={productProperties[p.name]}
-							onChange={(ev) => setProductProp(p.name, ev.target.value)}>
-							{p.values.map((v, idx) => (
-								<option key={idx} value={v}>
-									{v}
-								</option>
-							))}
-						</select>
+					<div key={index} className=''>
+						<label>{p.name[0].toUpperCase() + p.name.substring(1)}</label>
+						<div>
+							<select
+								value={productProperties[p.name]}
+								onChange={(ev) => setProductProp(p.name, ev.target.value)}>
+								{p.values.map((v, idx) => (
+									<option key={idx} value={v}>
+										{v}
+									</option>
+								))}
+							</select>
+						</div>
 					</div>
 				))}
 			<label>Photos</label>
@@ -143,7 +145,7 @@ export default function ProductForm({
 						images.map((image) => (
 							<div
 								key={image.id}
-								className='h-24 w-24 relative rounded-lg overflow-hidden'>
+								className='h-24 w-24 bg-white p-4 shadow-sm relative rounded-sm border border-gray-200 overflow-hidden'>
 								<Image
 									src={image.src}
 									alt='any image'
@@ -158,7 +160,7 @@ export default function ProductForm({
 						<Spinner />
 					</div>
 				)}
-				<label className='w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200'>
+				<label className='w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-primary rounded-sm bg-white shadow-sm border border-primary'>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
 						fill='none'
@@ -172,7 +174,7 @@ export default function ProductForm({
 							d='M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5'
 						/>
 					</svg>
-					<div>Upload</div>
+					<div>Add Image</div>
 					<input type='file' onChange={uploadImages} className='hidden' />
 				</label>
 			</div>
